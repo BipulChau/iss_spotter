7d0f079dfe10f94f9a01b45410ecb18e4f1a8383
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-23 21:28:03 -0400

    fetchIssFlyOverTimes function is complete

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-// const { fetchMyIP,
-//   fetchCoordsByIP} = require('./iss.js');
+const { fetchMyIP,
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes} = require('./iss.js');
+
+
+const coords = { latitude: 43.7282, longitude: -79.2323 };
+
+fetchISSFlyOverTimes(coords, (error, data) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  return console.log("It worked! Returned flyover times", data);
+});
+
 
 // fetchCoordsByIP('174.91.121.52', (error, data) => {
 //   if (error) {
