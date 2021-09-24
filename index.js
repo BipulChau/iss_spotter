a8b7c1d5a2e2e590749b3212a9aa8480d0989a19
Bipul Chaudhary <bipulchau@gmail.com>
2021-09-23 20:08:15 -0400

    Fetch my ip is complete

---
+++ index.js
@@ -0,0 +1,12 @@
+const { fetchMyIP } = require('./iss.js');
+
+
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//    return  console.log("It didn't work!" , error);
+//     ;
+//   }
+
+//   return console.log('It worked! Returned IP:' , ip);
+// });
